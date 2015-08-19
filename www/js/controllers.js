57f931d9bb92efeda12b817e5b11c863134f3458
hellsalve017196 <abdullah017196@gmail.com>
2015-08-20 00:06:26 +0600

    after swipe function

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,40 +1,48 @@
 app.controller('checkCtrl',function($scope,$cordovaGeolocation,synchronousService,$state) {
-            $scope.imageSrc = 'file:///android_asset/www/img/tap.png';
+    $scope.imageSrc = 'file:///android_asset/www/img/tap.png';
 
-            $scope.zn = function() {
+    $scope.zn = function() {
 
-                $scope.imageSrc = 'file:///android_asset/www/img/req.png';
+        $scope.imageSrc = 'file:///android_asset/www/img/req.png';
 
-                var posOptions = {timeout: 10000, enableHighAccuracy: false};
+        var posOptions = {timeout: 10000, enableHighAccuracy: false};
 
-                $cordovaGeolocation.getCurrentPosition(posOptions)
-                    .then(function (position) {
+        $cordovaGeolocation.getCurrentPosition(posOptions)
+            .then(function (position) {
 
-                        var lat  = position.coords.latitude.toFixed(6);
-                        var long = position.coords.longitude.toFixed(6);
+                var lat  = position.coords.latitude.toFixed(6);
+                var long = position.coords.longitude.toFixed(6);
 
-                        url = 'http://agatafeedmill.com/maps/index.php/main/insert_geolocation/'+lat+'/'+long;
+                url = 'http://agatafeedmill.com/maps/index.php/main/insert_geolocation/'+lat+'/'+long;
 
-                        data = JSON.parse(synchronousService(url));
+                data = synchronousService(url);
 
-                        if(data.flag == 1)
-                        {
-                            $state.go('success');
-                        }
-                        else
-                        {
-                            alert("error occured")
-                        }
+                if(data == 1)
+                {
+                    $state.go('success');
+                }
+                else
+                {
+                    alert("error occured")
+                }
 
-                        console.log(data);
-                    }, function(err) {
-                        if(err.code == 2)
-                        {
-                            alert("check your internet setting or datetime setting");
-                        }
+                $scope.imageSrc = 'file:///android_asset/www/img/tap.png';
 
-                        $scope.imageSrc = 'file:///android_asset/www/img/tap.png';
-                    });
+                console.log(data);
+            }, function(err) {
+		console.log(JSON.stringify(err));
+		
+                alert("Please turn on your location services and connect to the internet");
 
-            }
-    });
+
+                $scope.imageSrc = 'file:///android_asset/www/img/tap.png';
+            });
+
+    }
+});
+
+app.controller('helpCtrl',function($scope,$state) {
+    $scope.home = function() {
+        $state.go('checkin');
+    }
+});
